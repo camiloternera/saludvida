2085235113eecb54a7f08986cd58a4e225253246
Camilo Ternera <camiloterneraduque@gmail.com>
2021-07-05 00:19:00 -0500

    Login starting

---
+++ auth_login.php
@@ -0,0 +1,49 @@
+<?php require "includes/config/database.php";  ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vidad - Salud</title>
+    <!-- CSS && Normalize -->
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/auth_login.css">
+</head>
+<body>
+    <div class="container">
+
+        <div class="title">
+            <div class="img-logo">
+                <img src="img/logo_saludvida.png" alt="Logo Salud Vida" class="logo">
+            </div>
+            <!-- /.img-logo -->
+
+            <div class="header-text">
+                <h1 class="header-text__text-login">Iniciar sesión</h1>
+                <h2 class="header-text__welcome">Bienvenido</h2>
+            </div>
+            <!-- /.header-text -->
+        </div>
+        <!-- /.title -->
+
+        <!-- ===== Form ===== -->
+        <div class="form-login">
+            <form class="form">
+                <input placeholder="Ingrese su cédula" class="input form__id" type="text" name="user" id="user">
+                <input placeholder="Ingrese su contraseña" class="input form__password" type="password" name="password" id="password">
+                <div class="form__forget-password">
+                    <a href="recover_password.html" class="text-forget-password">¿Olvidó su contraseña?</a>
+                </div>
+                <!-- /.forget-password -->
+
+                <input type="submit" value="Iniciar sesión">
+            </form>
+            <!-- /.form -->
+        </div>
+        <!-- /.form-login -->
+
+    </div>
+    <!-- /.container -->
+</body>
+</html>
