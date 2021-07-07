798672cd1ce2f38dac325f3efc18999ac17494f8
Diego Polo <80282895+Colerico02@users.noreply.github.com>
2021-07-07 02:09:50 -0500

    actualizacion de carpetas despues del merge con dashboard medico

---
+++ backend/auth_login.php
@@ -0,0 +1,32 @@
+<?php 
+
+    $cedula = $_POST['cedula'];
+    $password = $_POST['password'];
+
+    if(!empty($_POST['cedula']) && !empty($_POST['password'])){
+        require_once('includes/config/database.php');
+        $db = conectarDB();
+        
+        $query = "SELECT * FROM usuarios WHERE identificacion = '$cedula' and password_user = '$password'" ;
+        $resolve = mysqli_query($db , $query);
+        $rows = mysqli_num_rows($resolve);
+        if($rows > 0){
+            $user = mysqli_fetch_array($resolve);
+            session_start();
+            $_SESSION['user'] = $_POST['cedula'];
+            $_SESSION['rol'] = $user['id_rol'];
+
+            if($user['id_rol'] == 3)       
+                 header("location:../paciente.php");   
+            else if($user['id_rol'] == 2)
+                   header("location:../medico.php");            
+        }
+        else header("location:../login.php"); 
+
+    }
+    else header("location:../login.php");
+
+
+
+
+?>
---
+++ backend/includes/config/database.php
@@ -0,0 +1,19 @@
+<?php
+    // Conexion a la base de datos
+    function conectarDB() {
+        $host = "localhost";
+        $user = "root";
+        $password = "admin"; 
+        $database = "saludvida";
+
+        $db = mysqli_connect($host, $user, $password, $database);
+        if ($db) {
+            echo "Connect succefully";
+        } else {
+            echo "Danger! not conection";
+        }
+
+        return $db;
+    }
+
+?>
